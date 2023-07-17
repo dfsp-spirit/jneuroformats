9b01ebcc2bb050b847eb47bdd7e9b2c5552bbc50
Tim Schaefer <ts+code@rcmd.org>
2023-07-17 17:37:27 +0200

    CHG: change license headers in source files

--- jneuroformats/src/main/java/org/rcmd/jneuroformats/IO/IOUtil.java
+++ jneuroformats/src/main/java/org/rcmd/jneuroformats/IO/IOUtil.java
@@ -1,3 +1,8 @@
+/*
+ *  This file is part of jneuroformats and subject to its license.
+ *  See the LICENSE file that accompanies this distribution, or visit
+ *  the project homepage at https://github.com/dfsp-spirit/jneuroformats.
+ */
 
 package org.rcmd.jneuroformats.IO;
 
