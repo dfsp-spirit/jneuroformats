7cee83fbab98388a65e25a0e467c5b4c4a2ca41f
Tim Schaefer <ts+code@rcmd.org>
2023-07-20 13:22:43 +0200

    NEW: add test for writing and re-reading label

--- jneuroformats/src/test/java/org/rcmd/jneuroformats/FsLabelTest.java
+++ jneuroformats/src/test/java/org/rcmd/jneuroformats/FsLabelTest.java
@@ -8,7 +8,7 @@ package org.rcmd.jneuroformats;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.nio.file.Files;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -33,6 +33,28 @@ public class FsLabelTest {
             throw new RuntimeException(e);
         }
         assertThat(cortex.size()).isEqualTo(140891);
+    }
+
+    @Test
+    public void oneCanWriteAndRereadFsLabel() {
+
+        Path labelFile = Paths.get("src", "test", "resources", "subjects_dir", "subject1", "label", "lh.cortex.label");
+        FsLabel cortex;
+        try {
+            cortex = FsLabel.fromFsLabelFile(labelFile);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            Path temp = Files.createTempFile("", ".tmp");
+            cortex.writeToFile(temp, "fslabel");
+            FsLabel cortex2 = FsLabel.fromFsLabelFile(temp);
+            assertThat(cortex2.size()).isEqualTo(cortex.size());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
