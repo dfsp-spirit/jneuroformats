f6e836de023f657dd659c38a9a9a6d35bdddb9c1
Tim Schaefer <ts+code@rcmd.org>
2023-07-23 16:57:23 +0200

    CHG: cleanup imports and file headers

--- jneuroformats/src/main/java/org/rcmd/jneuroformats/FsColortable.java
+++ jneuroformats/src/main/java/org/rcmd/jneuroformats/FsColortable.java
@@ -1,15 +1,28 @@
+/*
+ *  Copyright 2023 Tim Schäfer
+ *
+ *    Licensed under the MIT License (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        https://github.com/dfsp-spirit/jneuroformats/blob/main/LICENSE or at https://opensource.org/licenses/MIT
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+
 package org.rcmd.jneuroformats;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.MessageFormat;
-import java.nio.channels.WritableByteChannel;
-import java.nio.file.StandardOpenOption;
 
 
 import org.rcmd.jneuroformats.IO.IOUtil;
--- jneuroformats/src/main/java/org/rcmd/jneuroformats/IO/IOUtil.java
+++ jneuroformats/src/main/java/org/rcmd/jneuroformats/IO/IOUtil.java
@@ -1,7 +1,17 @@
 /*
- *  This file is part of jneuroformats and subject to its license.
- *  See the LICENSE file that accompanies this distribution, or visit
- *  the project homepage at https://github.com/dfsp-spirit/jneuroformats.
+ *  Copyright 2023 Tim Schäfer
+ *
+ *    Licensed under the MIT License (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        https://github.com/dfsp-spirit/jneuroformats/blob/main/LICENSE or at https://opensource.org/licenses/MIT
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package org.rcmd.jneuroformats.IO;
